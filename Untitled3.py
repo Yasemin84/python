c6249922bf4a7bcfe7f0439918b71272044b93d6
Yasemin84 <127182187+Yasemin84@users.noreply.github.com>
2023-03-07 16:07:13 +0300

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+float (8)
+
+
+# In[2]:
+
+
+int(5.6)
+
+
+# In[3]:
+
+
+int(5.8)
+
+
+# In[4]:
+
+
+int(5.1)
+
+
+# In[5]:
+
+
+float(77)
+
+
+# In[6]:
+
+
+int(89.75)
+
+
+# In[7]:
+
+
+int(99.99)
+
+
+# In[8]:
+
+
+Sozluk1={"bir":1,"iki":2,"üç":3}
+
+
+# In[9]:
+
+
+Sozluk1["bir"]
+
+
+# In[10]:
+
+
+Sozluk={"table":"masa","orange":"portakal","beautiful":"güzel"}
+
+
+# In[11]:
+
+
+Sozluk["table"]
+
+
+# In[12]:
+
+
+Sozluk["beautiful"]
+
+
+# In[13]:
+
+
+a="Yasemin"
+a[3]
+
+
+# In[14]:
+
+
+a[2:6]
+
+
+# In[15]:
+
+
+a[2:-3]
+
+
+# In[ ]:
+
+
+
+
